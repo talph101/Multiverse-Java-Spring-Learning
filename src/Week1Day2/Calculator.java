9288e2587a626c627dbaec52579a1d9a1bc31bd5
Tiffany <139176913+talph101@users.noreply.github.com>
2024-10-15 14:13:44 -0500

    Refactored Calculator.

--- src/Week1Day2/Calculator.java
+++ src/Week1Day2/Calculator.java
@@ -1,21 +1,20 @@
 package Week1Day2;
 
 public class Calculator {
+    //This program calculates numbers
     int firstNum;
     int secondNum;
 
-    public Calculator (int numOne, int numTwo) {
-        firstNum = numOne;
-        secondNum = numTwo;
+    public Calculator () {
     }
 
-    public int addNums() {
-        int added = firstNum + secondNum;
+    public int add(int a, int b) {
+        int added = a + b;
         return added;
     }
 
-    public int subNums() {
-        int sub = firstNum - secondNum;
+    public int subtract(int c, int d) {
+        int sub = c - d;
         return sub;
     }
 
@@ -26,12 +25,12 @@ public class Calculator {
 
 
     public static void main(String [] args) {
-        Calculator firstCalculator = new Calculator(2,3);
-        Calculator secondCalculator = new Calculator(10,3);
+        Calculator firstCalculator = new Calculator();
+        Calculator secondCalculator = new Calculator();
 
 
-        System.out.println(firstCalculator.addNums());
-        System.out.println(secondCalculator.subNums());
+        System.out.println(firstCalculator.add(2,3));
+        System.out.println(secondCalculator.subtract(8,4));
         secondCalculator.multNums();
 
     }
